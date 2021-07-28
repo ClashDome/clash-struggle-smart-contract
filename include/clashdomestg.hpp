05adc9bf03a3aa7f32915c704e7f51460e1aed61
Enriqueto <enrique@ravalmatic.com>
2021-07-28 15:04:53 +0200

    refatoring

--- include/clashdomestg.hpp
+++ include/clashdomestg.hpp
@@ -31,7 +31,6 @@ public:
    /*
       System Actions
    */
-   [[eosio::on_notify("eosio.token::transfer")]] void deposit(name from, name to, asset amount, string memo);
    [[eosio::on_notify("atomicassets::transfer")]] void blenderize(name from, name to, vector<uint64_t> asset_ids, string memo);
 
    clashdomestg(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
@@ -109,33 +108,6 @@ private:
    rammarket _rammarket;
 
    // Private Functions
-
-   /*
-      Call eosio contract to sell RAM
-   */
-   void sellram(int64_t bytes)
-   {
-      action(
-          permission_level{CONTRACTN, name("active")},
-          "eosio"_n,
-          "sellram"_n,
-          make_tuple(CONTRACTN, bytes))
-          .send();
-   }
-
-   /*
-      Refund RAM value to collection owner
-   */
-   void refund(name owner, asset amount, string memo) {
-
-      action(
-          permission_level{CONTRACTN, name("active")},
-          "eosio.token"_n,
-          "transfer"_n,
-          make_tuple(CONTRACTN, owner, amount, memo))
-          .send();
-   }
-
    /*
       Check if user is authorized to mint NFTs
    */
@@ -181,42 +153,5 @@ private:
                 make_tuple(CONTRACTN, *it))
             .send();
         }
-   }
-
-   /*
-      Call to eosio contract to buy RAM and update user amount
-      (150 bytes every AtomicAsset NFT)
-   */
-   void buyramproxy(name collection, asset quantity) {
-
-      // Call eosio contract to buy RAM
-      action(
-          permission_level{CONTRACTN, name("active")},
-          "eosio"_n,
-          "buyram"_n,
-          make_tuple(CONTRACTN, CONTRACTN, quantity))
-          .send();
-            
-      // Get RAM price to update balances
-      double payRam = quantity.amount * RAMFEE;
-      auto itrRamMarket = _rammarket.begin();
-      double costRam = itrRamMarket->quote.balance.amount / itrRamMarket->base.balance.amount;
-      uint64_t quotaRam = uint64_t(payRam / costRam);
-      
-      // Update user RAM amount
-      auto itrBalance = _rambalance.find(collection.value);
-      if (itrBalance == _rambalance.end())
-      {
-         _rambalance.emplace(_self, [&](auto &rec) {
-            rec.collection = collection;
-            rec.bytes = quotaRam;
-         });
-      }
-      else
-      {
-         _rambalance.modify(itrBalance, _self, [&](auto &rec) {
-            rec.bytes = itrBalance->bytes + quotaRam;
-         });
-      }
    }
 };
--- src/clashdomestg.cpp
+++ src/clashdomestg.cpp
@@ -1,38 +1,6 @@
 #include <clashdomestg.hpp>
 
 /*
-   Listen to WAX reception
-   Check memo and sender to evaluate action
-      Is a RAM refund?
-      Is a deposit to buy RAM?
-*/
-[[eosio::on_notify("eosio.token::transfer")]] void clashdomestg::deposit(name from, name to, asset amount, string memo) {
-
-   if (from != CONTRACTN) {
-      // Does it come from RAM sale?
-      if (from == name("eosio.ram")) {
-         amount.amount = amount.amount * double(RAMFEE);
-         // Read users pending RAM refund
-         auto itrPendingRam = _pending_ram.begin();
-         refund(itrPendingRam->owner, amount, "Ram refund.");
-         _pending_ram.erase(itrPendingRam);
-      } else {
-         // Is it a deposit? check memo
-         check(memo.size() <= 12, "Wrong name Collection!"); // too much text
-         name collection = name(memo);
-
-         // Check if collection exists
-         atomicassets::collections_t _collections(ATOMIC, ATOMIC.value);
-         auto itrCollection = _collections.find(collection.value);
-         check(itrCollection != _collections.end(), "Collection not exist!");
-
-         // buy RAM
-         buyramproxy(collection, amount);
-      }
-   }
-}
-
-/*
    Create a blend or modify mixture templates
 */
 ACTION clashdomestg::createblend(name authorized_user, name target_collection, int32_t target_template, vector<int32_t> templates_to_mix) {
@@ -78,57 +46,49 @@ ACTION clashdomestg::createblend(name authorized_user, name target_collection, i
    // ignore NFTs sends by this smart contract. IMPORTANT!
    if (from != CONTRACTN) {
 
-      check(memo.size() < 100, "wrong memo!");
-      uint32_t target = stol(memo);
+        check(memo.size() < 100, "wrong memo!");
+        uint32_t target = stol(memo);
 
-      // Check if blend exists
-      auto itrBlender = _blenders.require_find(target, "Error 02: There is no blender for this target!");
+        // Check if blend exists
+        auto itrBlender = _blenders.require_find(target, "Error 02: There is no blender for this target!");
 
-      // Check if this smart contract is authorized
-      check(isAuthorized(itrBlender->collection, itrBlender->owner), "Error 03: The collection owner has disavowed this account!!");
+        // Check if this smart contract is authorized
+        check(isAuthorized(itrBlender->collection, itrBlender->owner), "Error 03: The collection owner has disavowed this account!!");
 
-      // Get id templates from NFTs received
-      vector<int32_t> mainMixtures = itrBlender->mixture;
-      vector<int32_t> blendTemplates = {};
-      atomicassets::assets_t _assets = atomicassets::assets_t(ATOMIC, get_self().value);
-      auto itrAsset = _assets.begin();
-      // for (auto it = asset_ids.begin(); it != asset_ids.end(); it++)
-      for (size_t i = 0; i < asset_ids.size(); i++) {
-         // itrAsset = _assets.find(*it);
-         itrAsset = _assets.find(asset_ids[i]);
-         blendTemplates.push_back(itrAsset->template_id);
-      }
+        // Get id templates from NFTs received
+        vector<int32_t> mainMixtures = itrBlender->mixture;
+        vector<int32_t> blendTemplates = {};
+        atomicassets::assets_t _assets = atomicassets::assets_t(ATOMIC, get_self().value);
 
-      // Check if mixture matches
-      sort(blendTemplates.begin(), blendTemplates.end()); // Make sure that vectors are sorted
-      sort(mainMixtures.begin(), mainMixtures.end());
-      check(blendTemplates == mainMixtures, "Error 05: There is no blender with these mixtures!");
+        auto itrAsset = _assets.begin();
+        // for (auto it = asset_ids.begin(); it != asset_ids.end(); it++)
+        for (size_t i = 0; i < asset_ids.size(); i++) {
+            // itrAsset = _assets.find(*it);
+            itrAsset = _assets.find(asset_ids[i]);
+            blendTemplates.push_back(itrAsset->template_id);
+        }
 
-      // Check RAM user balance
-      auto itrBalance = _rambalance.require_find(itrBlender->collection.value, "Error 18: Collection not found!");
-      check(itrBalance->bytes > 151, "Error 06: There is not enough RAM in this collection!");
+        // Check if mixture matches
+        sort(blendTemplates.begin(), blendTemplates.end()); // Make sure that vectors are sorted
+        sort(mainMixtures.begin(), mainMixtures.end());
+        check(blendTemplates == mainMixtures, "Error 05: There is no blender with these mixtures!");
 
-      // Check collecton mint limit and supply
-      atomicassets::templates_t _templates = atomicassets::templates_t(ATOMIC, itrBlender->collection.value);
-      auto itrTemplate = _templates.require_find(itrBlender->target, "Error 17: No template found!");
-      check(itrTemplate->max_supply > itrTemplate->issued_supply || itrTemplate->max_supply == 0, "Error 07: This blender cannot mint more assets for that target!");
+        // Check collecton mint limit and supply
+        atomicassets::templates_t _templates = atomicassets::templates_t(ATOMIC, itrBlender->collection.value);
+        auto itrTemplate = _templates.require_find(itrBlender->target, "Error 17: No template found!");
+        check(itrTemplate->max_supply > itrTemplate->issued_supply || itrTemplate->max_supply == 0, "Error 07: This blender cannot mint more assets for that target!");
 
-      // All right; let's blend and burn
-      mintasset(itrBlender->collection, itrTemplate->schema_name, itrBlender->target, from);
-      burnmixture(asset_ids);
-
-        // Update RAM balance (1 NFT = 151 bytes)
-      _rambalance.modify(itrBalance, _self, [&](auto &rec) {
-        rec.bytes = itrBalance->bytes - 151;
-      });
+        // All right; let's blend and burn
+        mintasset(itrBlender->collection, itrTemplate->schema_name, itrBlender->target, from);
+        burnmixture(asset_ids);
    }
 }
 
 /*
    Call to erase blend from tables
 */
-ACTION clashdomestg::delblend(name authorized_account, int32_t target_template)
-{
+ACTION clashdomestg::delblend(name authorized_account, int32_t target_template) {
+    
    require_auth(authorized_account);
 
    auto itrBlender = _blenders.require_find(target_template, "Error 01: No template with this ID!");
